0c3220aef981b6d8575b6d2c4ef8e0335213fe7c
Lesedi <leseditumelo32@gmail.com>
2022-01-26 20:06:39 +0200

    OCA EXAM

---
+++ Assesment Test/src/main/java/com/example/_C.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class _C {
+    private static int $;
+
+    public static void main(String[] main) {
+        String a_b = null;
+        System.out.print($);
+        System.out.print(a_b);
+    }
+}
+
